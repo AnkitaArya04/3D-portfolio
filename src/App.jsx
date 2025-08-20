b7223da9ce481029f3342466b4694d02c4d4523b
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-20 18:56:37 +0530

    portfolio-website

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import Experience from "./sections/Experience";
 import TechStack from "./sections/TechStack";
+import Testimonials from "./sections/Testimonials";
+import Contact from "./sections/Contact";
 
 const App = () => {
   return (
@@ -16,6 +18,8 @@ const App = () => {
     <FeatureCards/>
     <Experience/>
     <TechStack/>
+    <Testimonials/>
+    <Contact/>
     </>
   )
 }
