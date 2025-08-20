824910bc69583d649cd1adae6c283c261befb2e7
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-20 14:10:07 +0530

    portfolio-website

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-
+const words = [
+    {text:'Ideas', imgPath:'/images/ideas.svg'},
+    {text:'Concepts', imgPath:'/images/concepts.svg'},
+]
 const Hero = () => {
   return (
-    <div>
-      hero
-    </div>
+    <section id="hero" className="relative overflow-hidden">
+        <div className="absolute top-0 left-0 z-10">
+             <img src="/images/bg.png" alt="" />
+        </div>
+          <div className="hero-layout">
+              {/* LEFT: Hero Content */}
+              <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+                <h1>
+                    Shaping 
+                     <span className="slide">
+                   <span className="wrapper">
+
+                    </span>
+                    </span>
+                   </h1>
+                <h1>into Real Projects</h1>
+                <h1>that Deliver Results</h1>
+              </div>
+          </div>
+        </header>
+              {/* RIGHT: 3D Model or Visual */}
+          </div>
+    </section>
   )
 }
 
